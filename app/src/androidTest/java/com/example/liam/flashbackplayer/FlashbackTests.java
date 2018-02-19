39f9c0c88f931e4268c035fc6ccd0a47a7d1712a
JACKIE <gzxzk96@gmail.com>
2018-02-18 17:53:03 -0800

    issue 41 flashback algorithm testing

--- app/src/androidTest/java/com/example/liam/flashbackplayer/FlashbackTests.java
+++ app/src/androidTest/java/com/example/liam/flashbackplayer/FlashbackTests.java
@@ -10,15 +10,22 @@ import android.support.test.rule.GrantPermissionRule;
 import org.junit.Rule;
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.PriorityQueue;
+
+import static org.junit.Assert.assertEquals;
+
 public class FlashbackTests {
     @Rule
-    public ActivityTestRule<MainActivity> mainAct = new ActivityTestRule<MainActivity>(MainActivity.class) {
+    /*public ActivityTestRule<MainActivity> mainAct = new ActivityTestRule<MainActivity>(MainActivity.class) {
         @Override
         protected void beforeActivityLaunched() {
             clearSharedPrefs(InstrumentationRegistry.getTargetContext());
             super.beforeActivityLaunched();
         }
-    };
+    };*/
+    public ActivityTestRule<MainActivity> mainAct = new ActivityTestRule<MainActivity>(MainActivity.class);
+
     @Rule
     public GrantPermissionRule permissionRule = GrantPermissionRule.grant(android.Manifest.permission.ACCESS_FINE_LOCATION);
     @Rule
@@ -26,12 +33,33 @@ public class FlashbackTests {
     @Rule
     public GrantPermissionRule permissionRule3 = GrantPermissionRule.grant(Manifest.permission.WRITE_EXTERNAL_STORAGE);
 
+
     @Test
-    public void testLocationTimePref() {
+    public void testAlgorithm() {
         //somewhere in the Australian Outback, RIGHT NOW!
-        MockLocation mockLocation = new MockLocation(-27.516743, 134.342188);
+        ArrayList<Song> songArray= new ArrayList<Song>();
+
+        // 1    preference testing for algorithm
+        songArray.add(new MockSong(new SongLocation(134.342188, -27.516743),
+                0, 201702141503L, 0, 1 ));
+        songArray.add(new MockSong(new SongLocation(134.342188, -27.516743),
+                1, 201702141503L, 0, 1 ));
+        songArray.add(new MockSong(new SongLocation(134.342188, -27.516743),
+                2, 201702141503L, 0, 1 ));
+
+
+        FlashbackManager flashbackManager = new FlashbackManager(-27.516743,134.342188, 6, 6);
+        flashbackManager.rankSongs(songArray);
+        PriorityQueue<Song> pq = flashbackManager.getRankList();
+        assertEquals(2, pq.size());
+        Song popped = pq.poll();
+        assertEquals(1, pq.size());
+        assertEquals(2, popped.getPreference());
+
+
 
     }
+
 
 
     private void clearSharedPrefs(Context context) {
